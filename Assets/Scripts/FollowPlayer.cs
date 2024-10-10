b8dd8d846f94a79eae44504345d21e8390578ffd
Sparsh-I <sparsh.poonacha@gmail.com>
2024-10-10 12:49:57 -0700

    Knitby added
    Knitby game object added and follows Marshmallow around with a slight lag

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToFollow;
+    [SerializeField] private float timeOffset = 0.1f;
+    [SerializeField] private int granularity = 10;
+
+    [SerializeField] private float interpolationSpeed = 20;
+
+    private Queue<Vector3> path = new Queue<Vector3>();
+    private float queueTimer;
+
+    private Vector3 currentPathPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the objectToFollow has moved, this moves this GO towards the new position with speed = interpolationSpeed
+        if (currentPathPosition != Vector3.zero)
+            transform.position += (currentPathPosition - transform.position) * Time.deltaTime * interpolationSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        // granularity controls how many path positions can be stored in the queue before it is dequeued
+        // this controls how far back we want this GO to lag behind the objectToFollow
+        if (objectToFollow == null)
+            return;
+        queueTimer -= Time.fixedDeltaTime;
+
+        if (queueTimer <= 0)
+        {
+            queueTimer = timeOffset / granularity;
+            if (path.Count == granularity)
+                currentPathPosition = path.Dequeue();
+            path.Enqueue(objectToFollow.transform.position);
+        }
+    }
+}
