02ca65df7867c8d1d1c08683668e034ed50dd794
Edster1121 <eddieyoshie@gmail.com>
2024-10-22 21:07:36 -0700

    Paused Audio
    Audio paused when game paused. However, doesn't work after death because the game object is destroyed after death.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,95 +1,84 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// Singleton class for global game settings. Persists between scenes and reloads.
-/// </summary>
 public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
 
-    /// <summary>
-    /// Last checkpoint position. The player should respawn here if they die.
-    /// </summary>
     public Vector2 CheckPointPos { get; set; }
-
-    /// <summary>
-    /// Used between scene transitions. Set to false for respawns, true for transition between levels
-    /// </summary>
     private bool _newLevel;
-
     private bool _gamePaused;
 
+    // Array to hold references to all AudioSource components
+    private AudioSource[] _audioSources;
+
     private void Awake() {
-        if (Instance == null)
-        {
+        if (Instance == null) {
             _newLevel = true;
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            SceneManager.sceneLoaded += OnSceneLoaded; 
+            SceneManager.sceneLoaded += OnSceneLoaded;
+
+            // Get all AudioSource components in the AudioManager GameObject
+            _audioSources = FindObjectsOfType<AudioSource>();
         } else {
             Destroy(gameObject);
         }
     }
 
-    private void Update()
-    {
-        if (Input.GetButtonDown("Debug Reset"))
-        {
+    private void Update() {
+        if (Input.GetButtonDown("Debug Reset")) {
             Respawn();
         }
 
-        if (Input.GetButtonDown("Pause Game"))
-        {
+        if (Input.GetButtonDown("Pause Game")) {
             _gamePaused = !_gamePaused;
-            if (_gamePaused)
-            {
+            if (_gamePaused) {
                 PauseGame();
-            }
-            else
-            {
+            } else {
                 ResumeGame();
             }
         }
     }
 
-    void OnDestroy()
-    {
+    void OnDestroy() {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
-    
-    /// <summary>
-    /// On scene load, put player in the right spawn/respawn point
-    /// </summary>
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         var player = GameObject.FindGameObjectWithTag("Player");
-        if (_newLevel == false)
-        {
+        if (!_newLevel) {
             Vector3 spawnPos = new Vector3(CheckPointPos.x, CheckPointPos.y, player.transform.position.z);
             player.transform.position = spawnPos;
-        }
-        else
-        {
+        } else {
             CheckPointPos = player.transform.position;
         }
-        
+
         FollowCamera cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowCamera>();
         Vector2 playerTarget = cam.GetPlayerTarget();
         cam.transform.position = new Vector3(playerTarget.x, playerTarget.y, cam.transform.position.z);
     }
 
-    public void PauseGame ()
-    {
+    public void PauseGame() {
         Time.timeScale = 0;
+
+        // Mute all audio sources
+        foreach (var audioSource in _audioSources) {
+            audioSource.Pause();
+            audioSource.volume = 0;
+        }
     }
 
-    public void ResumeGame ()
-    {
+    public void ResumeGame() {
         Time.timeScale = 1;
+
+        // Restore audio volume
+        foreach (var audioSource in _audioSources) {
+            audioSource.volume = 1;
+            audioSource.UnPause();
+        }
     }
 
-    public void Respawn()
-    {
+    public void Respawn() {
         _newLevel = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
