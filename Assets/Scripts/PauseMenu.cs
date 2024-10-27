290f483aa404757768ef517f869977def3edd415
Edster1121 <eddieyoshie@gmail.com>
2024-10-26 23:52:04 -0700

    PauseMenu stops audio
    PauseMenu pauses the audio instead of GameManager.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -26,6 +26,7 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        AudioListener.volume = 1f;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
@@ -33,6 +34,7 @@ public class PauseMenu : MonoBehaviour
 
     void Pause()
     {
+        AudioListener.volume = 0f;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
