fec78e16b1e58498796c5c7de224483d0fc7d6cd
Eddie Yoshie <eddieyoshie@gmail.com>
2024-12-14 17:12:07 -0800

    Made MainMenu volume slider linear instead of logarithmic

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {	
 	public AudioMixer audioMixer;
+	public Slider volumeSlider;
+
+	public float startVolume = 0.5f;
+
+	public void Start()
+	{
+		SetVolume(startVolume);
+		volumeSlider.value = startVolume;
+	}
 
 	public void SetVolume(float volume)
 	{
-		audioMixer.SetFloat("Volume", volume);
+		audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
 	}
     public void PlayGame()
     {
-       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       SceneManager.LoadScene("PauseTest");
     }
     
     public void QuitGame()
