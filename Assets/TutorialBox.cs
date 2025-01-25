e13324b65fd321ace2414e86cf5517c20d5fda42
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-25 10:07:02 -0800

    fade in tutorial

--- Assets/TutorialBox.cs
+++ Assets/TutorialBox.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialBox : MonoBehaviour
 {
     private string _moveToShow;
-    [SerializeField] private Animator moveAnim;
-    [SerializeField] private Animator keyAnim;
+    private Animator _moveAnim;
+    private Animator _keyAnim;
+    private SpriteRenderer _moveSprite;
+    private SpriteRenderer _keySprite;
+    private Coroutine _fadeCoroutine;
+
+    [SerializeField] private GameObject moveDisplay;
+    [SerializeField] private GameObject keyDisplay;
+    [SerializeField] private float fadeDuration = 1.0f;
     
     // Dictionary of tutorial moves
     // Tuple items:
@@ -24,7 +32,15 @@ public class TutorialBox : MonoBehaviour
             {"swing", new Tuple<string, float, string, float>
                 ("Tutorial_Player_Swing", 0.25f, "Tutorial_Key_Space", 0.25f)}
         };
-    
+
+    private void Start()
+    {
+        _moveAnim = moveDisplay.GetComponent<Animator>();
+        _moveSprite = moveDisplay.GetComponent<SpriteRenderer>();
+        _keyAnim = keyDisplay.GetComponent<Animator>();
+        _keySprite = keyDisplay.GetComponent<SpriteRenderer>();
+    }
+
     public void SetMove(string move)
     {
         if (_moves.ContainsKey(move))
@@ -35,17 +51,71 @@ public class TutorialBox : MonoBehaviour
     
     public void ShowTutorial()
     {
-        moveAnim.enabled = true;
-        keyAnim.enabled = true;
-        moveAnim.Play(_moves[_moveToShow].Item1);
-        moveAnim.speed = _moves[_moveToShow].Item2;
-        keyAnim.Play(_moves[_moveToShow].Item3);
-        keyAnim.speed = _moves[_moveToShow].Item4;
+        _moveAnim.enabled = true;
+        _moveAnim.Play(_moves[_moveToShow].Item1);
+        _moveAnim.speed = _moves[_moveToShow].Item2;
+        
+        _keyAnim.enabled = true;
+        _keyAnim.Play(_moves[_moveToShow].Item3);
+        _keyAnim.speed = _moves[_moveToShow].Item4;
+
+        if (_fadeCoroutine != null)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+        _fadeCoroutine = StartCoroutine(FadeIn());
     }
 
     public void HideTutorial()
     {
-        moveAnim.enabled = false;
-        keyAnim.enabled = false;
+        if (_fadeCoroutine != null)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+        _fadeCoroutine = StartCoroutine(FadeOut());
+        
+        _moveAnim.enabled = false;
+        _keyAnim.enabled = false;
+    }
+    
+    private IEnumerator FadeIn()
+    {
+        float elapsedTime = 0f;
+        Color moveColor = _moveSprite.color;
+        Color keyColor = _keySprite.color;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            moveColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            keyColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            _moveSprite.color = moveColor;
+            _keySprite.color = keyColor;
+            yield return null;
+        }
+        moveColor.a = 1f;
+        keyColor.a = 1f;
+        _moveSprite.color = moveColor;
+        _keySprite.color = keyColor;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float elapsedTime = 0f;
+        Color moveColor = _moveSprite.color;
+        Color keyColor = _keySprite.color;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            moveColor.a = Mathf.Clamp01(1f - elapsedTime / fadeDuration);
+            keyColor.a = Mathf.Clamp01(1f - elapsedTime / fadeDuration);
+            _moveSprite.color = moveColor;
+            _keySprite.color = keyColor;
+            yield return null;
+        }
+
+        moveColor.a = 0f;
+        keyColor.a = 0f;
+        _moveSprite.color = moveColor;
+        _keySprite.color = keyColor;
     }
 }
