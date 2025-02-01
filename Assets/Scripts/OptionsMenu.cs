af7739bb2836e4d08d79c78d40bc08c3314ed349
Kai <kaisoapbox@gmail.com>
2025-01-31 19:50:56 -0800

    fix main menu options menu sliders

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -3,10 +3,9 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    
     [SerializeField] private Slider bgmSlider;
     [SerializeField] private Slider sfxSlider;
-    
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -14,5 +13,8 @@ public class OptionsMenu : MonoBehaviour
         var savedSfxVolume = PlayerPrefs.GetFloat("SFX");
         bgmSlider.value = savedBgmVolume;
         sfxSlider.value = savedSfxVolume;
+
+        bgmSlider.onValueChanged.AddListener(delegate { SoundManager.Instance.SetBgmVolume(bgmSlider.value); });
+        sfxSlider.onValueChanged.AddListener(delegate { SoundManager.Instance.SetSfxVolume(sfxSlider.value); });
     }
 }
