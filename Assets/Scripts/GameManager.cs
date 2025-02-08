7aa9bf6b1fd6ac0f7fdfd993e5a88fdbea740960
Jen Taruno <99060936+jentaruno@users.noreply.github.com>
2025-02-07 20:00:04 -0800

    New respawn system + refactor level 1 hierarchy (#34)
    * Move reset button input to game manager + add Esc to pause
    
    * Add files via upload
    
    * feat: modify pause
    
    * feat: oops debug
    
    * feat: revert pause changes
    
    * Paused Audio
    
    Audio paused when game paused. However, doesn't work after death because the game object is destroyed after death.
    
    * Implemented Janky Pause
    
    Pausing audio and physics implemented.
    
    Pause Menu does not work in this version, and audio keeps playing when pausing after death.
    
    * Added Main Menu
    
    Added a main menu with play, options, and quit buttons. Clicking play switches scenes to PauseTest. Options opens another menu called OptionsMenu. The volume slider is not functional.
    
    * Added PauseMenu
    
    * PauseMenu stops audio
    
    PauseMenu pauses the audio instead of GameManager.
    
    * Working Volume Slider on Main Menu
    
    Used Simon's first level music as the menu music as a placeholder and added the volume slider functionality on the main menu.
    
    * Added volume slider to pauseTest
    
    * Update .gitignore
    
    * misc(cleanup): remove .idea
    
    * fix gitignore
    
    * feat: make it load
    
    * Volume Slider Kind of Working
    
    Volume slider on main working. Volume slider on PauseTest Scene still plays walking,sliding sound effect on pause. Need to handle sound effects on pause. Also both sliders are still logarithmic, not linear yet. Linear requires some math which I have not figured out yet.
    
    * Pause Menu no longer appears when loading PauseTest Scene
    
    * Made MainMenu volume slider linear instead of logarithmic
    
    * Made some variables directly editable
    
    * Removed duplicate BGM audio
    
    * Stores volume across scenes, mutes SFX on pause
    
    * Split master volume to BGM and SFX
    
    BGM is fully functional, SFX is still in progress.
    
    * SFX slider functional
    
    * Delete textmeshpro folder
    
    * PauseMenu ready for review
    
    * Got rid of TextMeshPro folder
    
    * Camera works with pause, fixed frozen when switching scenes error
    
    * update scene
    
    * fix: handle pausing with dialogue
    
    * fix: make fields private, tweak whitespace
    
    * upload tutorial sprites
    
    * Tutorial box plays depending on what's set
    
    * IT APPEARS
    
    * fade in tutorial
    
    * Fade in and out tutorial
    
    * region based tutorial start and end
    
    * Split swing tutorial
    
    * Added comments to public methods, moved all sound related methods to singleton Sound Manager
    
    * Uncommented broken cutscene camera code
    
    * unify scene BGM handling in Manager prefab, do the right scene transitions
    
    * fix cutscene pause menu, add pausemenu prefab, load level1 instead
    
    * replace the lost meta files?
    
    * move pausemenu into game manager prefab
    
    * fix main menu options menu sliders
    
    * make it private
    
    * remove pause panel image
    
    * Added Master Slider
    
    * Added Master Slider V2
    
    * swing tutorial
    
    * fix master slider update thing idk
    
    * delete pause test again
    
    * splash screen
    
    * put player at start of level 1
    
    * update options menu appearance
    
    * reset works
    
    * fix null exception
    
    * checkpoint dir + cleanup death smoke on respawn
    
    * Fix Knitby respawn, make resetter a tag
    
    * docu + give up on parallax bg reset
    
    * Fade in and out
    
    * fade effects
    
    * merge
    
    * docu
    
    * add fade canvas to manager prefab
    
    * cleanup hierarchy
    
    * apply prefabs
    
    ---------
    
    Co-authored-by: Eddie Yoshie <78448256+Edster1121@users.noreply.github.com>
    Co-authored-by: Kai <kaisoapbox@gmail.com>
    Co-authored-by: Edster1121 <eddieyoshie@gmail.com>
    Co-authored-by: AdenC123 <39308648+AdenC123@users.noreply.github.com>

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
 
 /// <summary>
-///     Singleton class for global game settings. Persists between scenes and reloads.
+/// Singleton class for global game settings. Persists between scenes and reloads.
 /// </summary>
 public class GameManager : MonoBehaviour
 {
-    /// <summary>
-    ///     Used between scene transitions. Set to false for respawns, true for transition between levels
-    /// </summary>
-    private bool _newLevel;
-
     public static GameManager Instance { get; private set; }
 
     /// <summary>
-    ///     Last checkpoint position. The player should respawn here if they die.
+    /// Last checkpoint position. The player should respawn here if they die.
     /// </summary>
     public Vector2 CheckPointPos { get; set; }
+    
+    /// <summary>
+    /// If true, the player faces left when they respawn
+    /// </summary>
+    public bool RespawnFacingLeft { get; set; }
+
+    /// <summary>
+    /// Object that contains all objects that need to be reset when the player respawns
+    /// </summary>
+    private List<Resettable> _resetters = new();
+
+    /// <summary>
+    /// Canvas to fade in/out when transitioning between scenes
+    /// </summary>
+    [SerializeField] private FadeEffects sceneTransitionCanvas;
 
     private void Awake()
     {
         if (Instance == null)
         {
-            _newLevel = true;
             Instance = this;
             DontDestroyOnLoad(gameObject);
             SceneManager.sceneLoaded += OnSceneLoaded;
@@ -44,37 +55,29 @@ public class GameManager : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
-    /// <summary>
-    ///     On scene load, put player in the right spawn/respawn point
-    /// </summary>
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        sceneTransitionCanvas.InvokeFadeOut();
         Time.timeScale = 1f;
-        var player = GameObject.FindGameObjectWithTag("Player");
-        var knitby = GameObject.FindGameObjectWithTag("Knitby");
-        if (!player || !knitby) return;
-
-        if (_newLevel == false)
-        {
-            var spawnPos = new Vector3(CheckPointPos.x, CheckPointPos.y, player.transform.position.z);
-            player.transform.position = spawnPos;
-            knitby.transform.position = spawnPos;
-        }
-        else
-        {
-            CheckPointPos = player.transform.position;
-        }
-
-        var cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowCamera>();
-        if (cam is null) return;
-        var playerTarget = cam.GetPlayerTarget();
-        cam.transform.position = new Vector3(playerTarget.x, playerTarget.y, cam.transform.position.z);
+        foreach (var resetObject in GameObject.FindGameObjectsWithTag("Resetter"))
+            _resetters.Add(resetObject.GetComponent<Resettable>());
     }
 
+    /// <summary>
+    /// Resets core components like player, Knitby, camera, etc to the state at
+    /// the last checkpoint
+    /// </summary>
     public void Respawn()
     {
-        _newLevel = false;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        sceneTransitionCanvas.FadeIn += OnFadeIn;
+        sceneTransitionCanvas.InvokeFadeInAndOut();
+    }
+
+    private void OnFadeIn()
+    {
+        foreach (var resetObject in _resetters)
+            resetObject.Reset();
+        sceneTransitionCanvas.FadeIn -= OnFadeIn;
     }
 
     [YarnCommand("load_scene")]
