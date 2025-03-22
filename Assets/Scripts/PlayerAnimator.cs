fe77794cc16085906365b6ba03b6abdf7989c111
slin04 <74470276+slin04@users.noreply.github.com>
2025-03-22 11:57:09 -0700

    fixed wallslide, added random jump, added dash sfx

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 ///     Handles player animation
@@ -22,7 +22,8 @@ public class PlayerAnimator : MonoBehaviour
     [Header("Audio Clips")] [SerializeField]
     private AudioClip runSound;
 
-    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip[] jumpSound;
+    [SerializeField] private AudioClip dashSound;
     [SerializeField] private AudioClip landSound;
     [SerializeField] private AudioClip wallSlideSound;
     [SerializeField] private AudioClip[] deathSounds;
@@ -66,6 +67,7 @@ public class PlayerAnimator : MonoBehaviour
         _player.GroundedChanged += OnGroundedChanged;
         _player.WallChanged += OnWallChanged;
         _player.Death += OnDeath;
+        _player.Dashed += OnDash;
 
         // _moveParticles.Play();
     }
@@ -136,8 +138,9 @@ public class PlayerAnimator : MonoBehaviour
         anim.SetTrigger(JumpKey);
         anim.SetBool(GroundedKey, false);
 
-        _source.clip = jumpSound;
-        _source.PlayOneShot(jumpSound);
+        int randomSound = Random.Range(0, jumpSound.Length);
+        _source.clip = jumpSound[randomSound];
+        _source.PlayOneShot(jumpSound[randomSound]);
 
         // if (_grounded) // Avoid coyote
         // {
@@ -197,6 +200,12 @@ public class PlayerAnimator : MonoBehaviour
             _source.PlayOneShot(sound);
         Instantiate(deathSmoke, transform);
         anim.SetBool(DeathKey, true);
+    }
+
+    private void OnDash()
+    {
+        _source.clip = dashSound;
+        _source.PlayOneShot(dashSound);
     }
 
     // private void DetectGroundColor()
