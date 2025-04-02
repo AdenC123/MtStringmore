99eb8ad9c7a0e76ec372ad776fc0e12940bfce1b
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-04-01 23:07:43 -0700

    1) add lv2 bg to cutscene 2, pan to it at the end. 2) make knitby idle anim slower

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -12,7 +12,13 @@ public class CutsceneManager : MonoBehaviour
 
     private void Awake()
     {
+        var allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        if (allAudioSources == null) return;
+        foreach (var audioSource in allAudioSources)
+            audioSource.Stop();
+        
         _source = GetComponent<AudioSource>();
+        _source.Play();
     }
 
     private void Update()
@@ -29,8 +35,31 @@ public class CutsceneManager : MonoBehaviour
     [YarnCommand("play_sound")]
     public static IEnumerator PlaySound(string soundName, bool blockUntilDone = true)
     {
+        if (_source.volume == 0)
+            _source.volume = 1;
+        
         _source.PlayOneShot(Resources.Load<AudioClip>(soundName));
         if (blockUntilDone)
             yield return new WaitUntil(() => !_source.isPlaying);
+    }
+
+    [YarnCommand("stop_sound")]
+    public static IEnumerator StopSound(float fadeDuration = 0)
+    {
+        if (fadeDuration == 0)
+        {
+            _source.Stop();
+            yield return null;
+        }
+        else
+        {
+            float elapsedTime = 0f;
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.unscaledDeltaTime;
+                _source.volume = Mathf.Lerp(_source.volume, 0, elapsedTime / fadeDuration);
+                yield return null;
+            }
+        }
     }
 }
