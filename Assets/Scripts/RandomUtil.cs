6a54f89b5a5bd2a36d47b992074d16a46958b36b
Z0rb14n <babybluewoof@gmail.com>
2025-04-04 20:53:31 -0700

    Implement swing sounds

---
+++ Assets/Scripts/RandomUtil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Utility class to select random objects.
+/// </summary>
+public static class RandomUtil
+{
+    /// <summary>
+    /// Gets the random object from an array.
+    /// </summary>
+    /// <param name="array">Array to get objects from</param>
+    /// <typeparam name="T">Type of objects in array</typeparam>
+    /// <returns>Random object</returns>
+    public static T SelectRandom<T>(T[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            return default;
+        }
+        int index = Random.Range(0, array.Length);
+        return array[index];
+    }
+}
