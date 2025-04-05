76852063a40cee3b12de6c07c00362944cf38ed2
Z0rb14n <babybluewoof@gmail.com>
2025-04-04 22:03:25 -0700

    actually that's not require

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Balloon : AbstractPlayerInteractable
 {
     [SerializeField, Tooltip("Acceleration curve over time, in [0, 1]")]
