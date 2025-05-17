6dd1328605f91d8ba64d756b9d9311b55727e6b2
Z0rb14n <babybluewoof@gmail.com>
2025-05-17 16:20:10 -0700

    Disable Load game button in main menu in webGL

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using Save;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Main menu canvas logic.
@@ -8,12 +9,17 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string startingScene;
+    [SerializeField] private Button loadGameButton;
 
     private SaveDataManager _saveDataManager;
 
     private void Awake()
     {
         _saveDataManager = FindObjectOfType<SaveDataManager>();
+#if UNITY_WEBGL
+        Debug.LogWarning("Saving and loading isn't supported on WebGL yet.");
+        Destroy(loadGameButton.gameObject);
+#endif
     }
 
     public void PlayGame()
