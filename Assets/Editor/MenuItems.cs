5bde54d9c549794a80b2c704ebefe1bfe3fbbef6
AdenC123 <adenrudy23@gmail.com>
2025-05-22 21:09:36 -0700

    collectable randomizer menu

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,25 @@
+﻿using Interactables;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    /// <summary>
+    /// Manager for editor menu items, etc.
+    /// </summary>
+    public class MenuItems : MonoBehaviour
+    {
+        /// <summary>
+        /// Randomize the rotation and sprite of all collectables in the current scene.
+        /// </summary>
+        [MenuItem("Assets/Randomize Collectables")]
+        private static void RandomizeAllCollectablesInScene()
+        {
+            foreach (Collectable collectable in GameObject.FindObjectsOfType<Collectable>())
+            {
+                collectable.RandomizeSprite();
+                collectable.RandomizeRotation();
+            }
+        }
+    }
+}
