b04076463538f3a4529e3ca60fee6c3d4b1cafee
Michael Sam <michael2g1317@gmail.com>
2025-05-29 19:29:41 -0700

    doing house keeping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static ShakeCamera;
 /// <summary>
 /// Controls player movement and invokes events for different player states
 /// </summary>
@@ -191,7 +190,7 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<CapsuleCollider2D>();
-        _shake = GameObject.FindWithTag("CameraEffector").GetComponent<ShakeCamera>();
+        _shake = FindObjectOfType<ShakeCamera>();
         _audioSource = GetComponent<AudioSource>();
         ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
 
@@ -533,7 +532,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (PlayerState is PlayerStateEnum.Dash)
         {
-            _shake.Shake(5f,10f,0.1f,true,true); //TODO: change temp parameters for visual testing
+            _shake.Shake(2f,10f,0.1f,true,true); //TODO: change temp parameters for visual testing
 
             // move player forward at dash speed
             _velocity.y = 0;
--- Assets/Scripts/ShakeCamera.cs
+++ Assets/Scripts/ShakeCamera.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static PlayerController;
 
 /// <summary>
 /// Adds a rumbling shake to the MainCamera
@@ -19,15 +18,12 @@ public class ShakeCamera : MonoBehaviour
     /// <summary>
     /// Public method that any class can shake the camera.
     /// </summary>
-    /// <param>
-    /// shakeDuration: the length of time to shake the camera for
-    /// shakeIntensity: the level of intensity to shake the camera (higher values cause more aggressive/violent shaking)
-    /// shakeDecay: how quickly the camera shake dissipates (large values cause shake to quickly dissapate)
-    /// xShake/yShake: biases the direction in which the camera should shake for
-    /// </param>
+    /// <param name = "shakeDuration">the length of time to shake the camera for</param>
+    /// <param name = "shakeIntensity">the level of intensity to shake the camera (higher values cause more aggressive/violent shaking)</param>
+    /// <param name = "shakeDecay">how quickly the camera shake dissipates (large values cause shake to quickly dissapate)</param>
+    /// <param name = "xShake/yShake">biases the direction in which the camera should shake for</param>
     public void Shake(float shakeDuration, float shakeIntensity, float shakeDecay, bool xShake, bool yShake) 
     {
-        //TODO: trying to prevent coroutines from overlapping need to fix
         if(_activeShake != null) 
         {
             transform.localPosition = _originalCameraPos;
@@ -40,35 +36,21 @@ public class ShakeCamera : MonoBehaviour
     {
         float _elapsed = 0.0f;  
         Vector2 _shakeOffset;
-        while (_elapsed < shakeDuration)  
+        for (float elapsed = 0; elapsed < shakeDuration; elapsed += Time.deltaTime)  
         {
-            if(xShake && yShake)    //shake camera both directions
-            {
-                //perlin noise used for smooth randomness
-                _shakeOffset = new Vector2(
-                    Mathf.PerlinNoise(Time.time * 10f, 0) * 2 - 1,  
-                    Mathf.PerlinNoise(Time.time * 10f, 1) * 2 - 1   
-                ) * shakeIntensity;
-            } else if(xShake)   //shake camera only in x direction
-            {
-                _shakeOffset = new Vector2(Mathf.PerlinNoise(Time.time * 10f, 0) * 2 - 1, 0) * shakeIntensity;
-            } else  //shake camera only in y direction
-            {
-                _shakeOffset = new Vector2(0,Mathf.PerlinNoise(Time.time * 10f, 1) * 2 - 1 ) * shakeIntensity;
-            }
+            Vector2 shakeOffset = Vector2.zero;
+            //perlin noise used for smooth randomness
+            if (xShake) shakeOffset.x = (Mathf.PerlinNoise1D(Time.time * 10f) * 2 - 1) * shakeIntensity;
+            if (yShake) shakeOffset.y = (Mathf.PerlinNoise(Time.time * 10f, 1) * 2 - 1) * shakeIntensity;
 
             // lerp between the original camera position and the shake offset
             transform.localPosition = Vector2.Lerp(
                 transform.localPosition,                            // start pos
-                _originalCameraPos + _shakeOffset,                   // target pos
+                _originalCameraPos + shakeOffset,                   // target pos
                 shakeDecay * Time.deltaTime                        // reduce shaking over time
             );
-
-            _elapsed += Time.deltaTime;
-            //yield return new WaitForSeconds(0.01f); //TODO this value might need tweaking based on performance issues
             yield return null;
         }
-
         transform.localPosition = _originalCameraPos;
         _activeShake = null;
         
