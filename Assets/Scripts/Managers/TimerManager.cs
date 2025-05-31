cfab4a0148063014fa14b9eca2d59f3afd8e5528
Sparsh-I <sparsh.poonacha@gmail.com>
2025-05-31 14:44:41 +0530

    Created togglable in game timer

--- Assets/Scripts/Managers/TimerManager.cs
+++ Assets/Scripts/Managers/TimerManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,25 +7,48 @@ namespace Managers
     public class TimerManager : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private TextMeshProUGUI inGameTimerText;
         [SerializeField] private GameObject resultsWindow;
         
-        // private float _elapsedGameTime;
         private float _elapsedLevelTime;
         
-        void Start()
+        // public bool IsResultsWindowActive => resultsWindow.activeSelf;
+        // public bool IsTimerShown => inGameTimerText.enabled;
+
+        private void Awake()
+        {
+            inGameTimerText.enabled = false;
+        }
+        
+        private void Start()
         {
             _elapsedLevelTime = 0;
         }
         
-        void Update()
+        private void Update()
         {
-            while (resultsWindow.activeSelf) return; 
+            bool previousState = inGameTimerText.enabled;
+            
+            if (resultsWindow.activeSelf)
+            {
+                inGameTimerText.enabled = false;
+                return;
+            }
+            
+            inGameTimerText.enabled = previousState;
             
             _elapsedLevelTime += Time.deltaTime;
             int minutes = (int) (_elapsedLevelTime / 60);
             int seconds = (int) (_elapsedLevelTime % 60);
             int milliseconds = (int) ((_elapsedLevelTime * 100) % 100);
+            
             timerText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+            inGameTimerText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+        }
+
+        public void OnToggle(bool isOn)
+        {
+            inGameTimerText.enabled = isOn;
         }
     }
 }
---
+++ Assets/Scripts/Managers/TimerToggleManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class TimerToggleManager : MonoBehaviour
+    {
+        [SerializeField] private Toggle toggle;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void Start()
+        {
+            OnSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+        }
+        
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            TimerManager timerManager = FindObjectOfType<TimerManager>();
+
+            if (timerManager != null)
+            {
+                toggle.onValueChanged.RemoveAllListeners();
+                toggle.onValueChanged.AddListener(timerManager.OnToggle);
+                timerManager.OnToggle(toggle.isOn);
+            }
+        }
+    }
+}
