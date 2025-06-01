a086d9ed671c1c921ee8164175fd4bcaf2216501
Eddie Yoshie <eddieyoshie@gmail.com>
2025-06-01 00:30:19 -0700

    done changes

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -1,114 +1,85 @@
-using System.Collections.Generic;
-using System.IO;
-using Save;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections.Generic;
 using Managers;
 
-public class LevelSelect : MonoBehaviour
+public class LevelSelectMenu : MonoBehaviour
 {
-    [SerializeField] private string level1;
-    [SerializeField] private string level2;
-    [SerializeField] private string level3;
-    [SerializeField] private string level4;
-    
-    [SerializeField] private Button level1button;
-    [SerializeField] private Button level2button;
-    [SerializeField] private Button level3button;
-    [SerializeField] private Button level4button;
+    public GameObject buttonPrefab;
+    public Transform buttonContainer;
+    public Button playButton;
 
-    private Color originalColor1 = Color.white;
-    private Color originalColor2 = Color.white;
-    private Color originalColor3 = Color.white;
-    private Color originalColor4 = Color.white;
-    private Color disabledColor = Color.gray;
+    public Sprite unlockedSprite;
+    public Sprite lockedSprite;
+    private Sprite selectedSprite;
+
+    [SerializeField] List<string> allLevelSceneNames;
+    
+    public List<string> unlockedScenes;
+
+    private string selectedScene = null;
+    private List<Button> levelButtons = new List<Button>();
 
     void Start()
     {
-        originalColor1 = level1button.GetComponent<Image>().color;
-        originalColor2 = level2button.GetComponent<Image>().color;
-        originalColor3 = level3button.GetComponent<Image>().color;
-        originalColor4 = level4button.GetComponent<Image>().color;
+        unlockedScenes = GameManager.Instance.levelsAccessed;
+        playButton.interactable = false;
 
-        DisableAllButtons();
-        UpdateLevelButtons();
-
-        if (GameManager.Instance != null)
-            GameManager.Instance.GameDataChanged += UpdateLevelButtons;
-    }
-
-    void OnDestroy()
-    {
-        if (GameManager.Instance != null)
-            GameManager.Instance.GameDataChanged -= UpdateLevelButtons;
-    }
-
-    void DisableAllButtons()
-    {
-        level1button.interactable = false;
-        level2button.interactable = false;
-        level3button.interactable = false;
-        level4button.interactable = false;
-
-        level1button.GetComponent<Image>().color = disabledColor;
-        level2button.GetComponent<Image>().color = disabledColor;
-        level3button.GetComponent<Image>().color = disabledColor;
-        level4button.GetComponent<Image>().color = disabledColor;
-    }
-
-    void UpdateLevelButtons()
-    {
-        List<string> levels = GameManager.Instance.levelsAccessed;
-        
-
-        foreach (string level in levels)
+        foreach (string sceneName in allLevelSceneNames)
         {
-            enableLevelButtons(level);
+            GameObject btnObj = Instantiate(buttonPrefab, buttonContainer);
+            Button button = btnObj.GetComponent<Button>();
+            TMP_Text label = btnObj.GetComponentInChildren<TMP_Text>();
+            Image background = btnObj.GetComponent<Image>();
+
+            bool isUnlocked = unlockedScenes.Contains(sceneName);
+            int levelNumber = allLevelSceneNames.IndexOf(sceneName) + 1;
+
+            if (isUnlocked)
+            {
+                label.text = levelNumber.ToString();
+                background.sprite = unlockedSprite;
+                button.interactable = true;
+
+                button.onClick.AddListener(() => OnLevelSelected(sceneName, button));
+            }
+            else
+            {
+                label.text = "";
+                background.sprite = lockedSprite;
+                button.interactable = false;
+            }
+
+            levelButtons.Add(button);
         }
+
+        playButton.onClick.AddListener(OnPlayClicked);
     }
 
-    void enableLevelButtons(string levelName)
+    void OnLevelSelected(string sceneName, Button clickedButton)
     {
-        if (levelName == level1)
+        selectedScene = sceneName;
+        playButton.interactable = true;
+
+        for (int i = 0; i < levelButtons.Count; i++)
         {
-            level1button.GetComponent<Image>().color = originalColor1;
-            level1button.interactable = true;
+            Image bg = levelButtons[i].GetComponent<Image>();
+            string thisScene = allLevelSceneNames[i];
+            bool isUnlocked = unlockedScenes.Contains(thisScene);
+
+            bg.sprite = isUnlocked ? unlockedSprite : lockedSprite;
         }
-        if (levelName == level2)
+        clickedButton.GetComponent<Image>().sprite = selectedSprite;
+    }
+
+    void OnPlayClicked()
+    {
+        if (!string.IsNullOrEmpty(selectedScene))
         {
-            level2button.GetComponent<Image>().color = originalColor2;
-            level2button.interactable = true;
+            Debug.Log("Loading selected level: " + selectedScene);
+            SceneManager.LoadScene(selectedScene);
         }
-        if (levelName == level3)
-        {
-            level3button.GetComponent<Image>().color = originalColor3;
-            level3button.interactable = true;
-        }
-        if (levelName == level4)
-        {
-            level4button.GetComponent<Image>().color = originalColor4;
-            level4button.interactable = true;
-        }
-    }
-
-    public void loadlevel1()
-    {
-        SceneManager.LoadScene(level1);
-    }
-
-    public void loadlevel2()
-    {
-        SceneManager.LoadScene(level2);
-    }
-
-    public void loadlevel3()
-    {
-        SceneManager.LoadScene(level3);
-    }
-
-    public void loadlevel4()
-    {
-        SceneManager.LoadScene(level4);
     }
 }
