ee86d0dadeb6fd654f9411b6db002eb77a4718d3
Eddie Yoshie <eddieyoshie@gmail.com>
2025-06-19 21:17:50 -0700

    did the thing

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -7,20 +7,33 @@ namespace Managers
     public class SoundManager : MonoBehaviour
     {
         private static SoundManager _instance;
-
         public static SoundManager Instance => _instance ??= FindObjectOfType<SoundManager>();
 
+        [Header("Audio Control")]
         [SerializeField] private AudioMixer audioMixer;
         [SerializeField] private Slider masterSlider;
         [SerializeField] private Slider bgmSlider;
         [SerializeField] private Slider sfxSlider;
-        [SerializeField, Range(float.Epsilon, 1)] private float startBgmVolume = 0.5f;
-        [SerializeField, Range(float.Epsilon, 1)] private float startSfxVolume = 0.5f;
-        [SerializeField, Range(float.Epsilon, 1)] private float startMasterVolume = 0.5f;
+        [SerializeField] private float startBgmVolume = 0.5f;
+        [SerializeField] private float startSfxVolume = 0.5f;
+        [SerializeField] private float startMasterVolume = 0.5f;
+
+        [Header("Collectable SFX")]
+        [SerializeField] private AudioClip[] collectableClips;
+        [SerializeField, Tooltip("Max time between collections to continue combo (in seconds)")]
+        private float collectableComboWindow = 1f;
+
+        private float _timeSinceLastCollect = Mathf.Infinity;
+        private int _audioIndex = 0;
+        private AudioSource _collectableAudioSource;
 
         private void Awake()
         {
             if (Instance != this) Destroy(gameObject);
+
+            _collectableAudioSource = gameObject.AddComponent<AudioSource>();
+            _collectableAudioSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups("SFX")[0];
+            DontDestroyOnLoad(gameObject);
         }
 
         private void Start()
@@ -33,12 +46,16 @@ namespace Managers
             SetSfxVolume(savedSfxVolume);
         }
 
+        private void Update()
+        {
+            _timeSinceLastCollect += Time.unscaledDeltaTime;
+        }
+
         public static float SliderToVolume(float sliderValue)
         {
             return Mathf.Log10(sliderValue) * 20;
         }
 
-        /// <summary> Sets Master volume (0.0001 to 1). </summary>
         public void SetMasterVolume(float volume)
         {
             masterSlider.value = volume;
@@ -46,7 +63,6 @@ namespace Managers
             PlayerPrefs.Save();
         }
 
-        /// <summary> Sets BGM volume (0.0001 to 1). </summary>
         public void SetBgmVolume(float volume)
         {
             audioMixer.SetFloat("BGM", SliderToVolume(volume));
@@ -55,7 +71,6 @@ namespace Managers
             PlayerPrefs.Save();
         }
 
-        /// <summary> Sets SFX volume (0.0001 to 1). </summary>
         public void SetSfxVolume(float volume)
         {
             audioMixer.SetFloat("SFX", SliderToVolume(volume));
@@ -64,11 +79,34 @@ namespace Managers
             PlayerPrefs.Save();
         }
 
-        /// <summary> Mutes or unmutes all audio. </summary>
         public void SetMute(bool isMuted)
         {
             audioMixer.SetFloat("Master",
                 isMuted ? -80f : SliderToVolume(PlayerPrefs.GetFloat("Master", startMasterVolume)));
+        }
+
+        /// <summary>
+        /// Plays a collectable sound in sequence with combo logic using PlayOneShot.
+        /// </summary>
+        public void PlayCollectableComboSound()
+        {
+            if (collectableClips == null || collectableClips.Length == 0) return;
+
+            if (_timeSinceLastCollect > collectableComboWindow)
+            {
+                _audioIndex = 0;
+            }
+            else
+            {
+                if (_audioIndex >= collectableClips.Length - 1)
+                    _audioIndex = collectableClips.Length - 1;
+                else
+                    _audioIndex++;
+
+            }
+
+            _collectableAudioSource.PlayOneShot(collectableClips[_audioIndex]);
+            _timeSinceLastCollect = 0f;
         }
     }
 }
