e5a1d7a82b003fd2a40754e01d8cb7531e486901
AdenC123 <adenrudy23@gmail.com>
2025-08-14 21:12:27 -0700

    namespaces and nits

--- Assets/Scripts/UI/OnSceneButtons.cs
+++ Assets/Scripts/UI/OnSceneButtons.cs
@@ -3,28 +3,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class OnSceneButtons : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private Button pauseButton;
-    [SerializeField] private Button restartButton;
-    
-    private void Awake()
+    public class OnSceneButtons : MonoBehaviour
     {
-        SceneManager.activeSceneChanged += OnSceneChanged;
-    }
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private Button restartButton;
     
-    private void OnDestroy()
-    {
-        SceneManager.activeSceneChanged -= OnSceneChanged;
-    }
+        private void Awake()
+        {
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
     
-    private void OnSceneChanged(Scene current, Scene next)
-    {
-        string thisScene = SceneManager.GetActiveScene().name;
-        bool isMainMenu = SceneListManager.Instance.IsMainMenu(thisScene);
-        bool isCutscene = SceneListManager.Instance.IsSceneCutscene(thisScene);
+        private void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnSceneChanged;
+        }
+    
+        private void OnSceneChanged(Scene current, Scene next)
+        {
+            string thisScene = SceneManager.GetActiveScene().name;
+            bool isMainMenu = SceneListManager.Instance.IsMainMenu(thisScene);
+            bool isCutscene = SceneListManager.Instance.IsSceneCutscene(thisScene);
 
-        pauseButton.gameObject.SetActive(!isMainMenu);
-        restartButton.gameObject.SetActive(!isMainMenu && !isCutscene);
+            pauseButton.gameObject.SetActive(!isMainMenu);
+            restartButton.gameObject.SetActive(!isMainMenu && !isCutscene);
+        }
     }
 }
