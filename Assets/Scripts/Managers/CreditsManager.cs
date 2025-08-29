c9a75451bd06e37c56615efb8d0d82ac17a548d9
Sparsh-I <sparsh.poonacha@gmail.com>
2025-08-29 11:46:48 -0700

    Removed temp dialogue and updated credits to main menu logic

--- Assets/Scripts/Managers/CreditsManager.cs
+++ Assets/Scripts/Managers/CreditsManager.cs
@@ -1,35 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Util;
-using Yarn.Unity;
 
 namespace Managers
 {
     public class CreditsManager : MonoBehaviour
     {
-        [SerializeField] private string mainMenuSceneName = "MainMenu";
-        private static bool _canExit;
-
-        void Start()
+        private void Update()
         {
-            _canExit = false;
-        }
-
-        void Update()
-        {
-            if (InputUtil.StartJumpOrTouch() && _canExit) LoadMainMenu();
-            
+            if (InputUtil.StartJumpOrTouch()) LoadMainMenu();
         }
 
         private void LoadMainMenu()
         {
-            SceneManager.LoadScene(mainMenuSceneName);
-        }
-        
-        [YarnCommand("enable_exit")]
-        public static void EnableExit()
-        {
-            _canExit = true;
+            SceneListManager.Instance.LoadMainMenu();
         }
     }
 }
