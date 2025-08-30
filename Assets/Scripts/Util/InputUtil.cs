b1a1d294cb2b2f13cba306886c33e4c6e8f59715
Z0rb14n <babybluewoof@gmail.com>
2025-08-30 13:26:41 -0700

    Revert useless changes

--- Assets/Scripts/Util/InputUtil.cs
+++ Assets/Scripts/Util/InputUtil.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 namespace Util
 {
     /// <summary>
-    ///     Input utility class.
+    /// Input utility class.
     /// </summary>
     public static class InputUtil
     {
         /// <summary>
-        ///     Whether a given screen position is on the pause button transform.
+        /// Whether a given screen position is on the pause button transform.
         /// </summary>
         /// <param name="screenPosition">Screen position</param>
         /// <returns></returns>
         private static bool OnPauseButton(Vector2 screenPosition)
         {
             return PauseMenu.Instance?.PauseButtonTransform &&
-                   RectTransformUtility.RectangleContainsScreenPoint(PauseMenu.Instance.PauseButtonTransform,
-                       screenPosition);
+                   RectTransformUtility.RectangleContainsScreenPoint(PauseMenu.Instance.PauseButtonTransform, screenPosition);
         }
-
+        
         /// <summary>
-        ///     Returns true if the player starts the jump button or there's any new touches in the screen.
+        /// Returns true if the player starts the jump button or there's any new touches in the screen.
         /// </summary>
         /// <returns>True if player started to jump (press space bar/touch screen)</returns>
         public static bool StartJumpOrTouch()
@@ -30,11 +29,11 @@ namespace Util
             return Input.GetButtonDown("Jump") ||
                    (Time.timeScale > 0 && (
                        (!OnPauseButton(Input.mousePosition) && Input.GetButtonDown("Fire1")) ||
-                       Input.touches.Any(touch => touch.phase == TouchPhase.Began && !OnPauseButton(touch.position))));
+                       (Input.touches.Any(touch => touch.phase == TouchPhase.Began && !OnPauseButton(touch.position)))));
         }
 
         /// <summary>
-        ///     Returns true if player's holding the jump action (i.e. holding space or holding touches).
+        /// Returns true if player's holding the jump action (i.e. holding space or holding touches).
         /// </summary>
         /// <returns>True if player holds the jump action</returns>
         public static bool HoldJumpOrTouch()
