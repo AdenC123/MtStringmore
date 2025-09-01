930b30b9d21eb188fd669217efbe6714b2cd416a
sidex18 <108185129+sidex18@users.noreply.github.com>
2025-08-31 21:52:24 -0700

    Level 4 end summit cutscene with fade in mechanics

---
+++ Assets/Scripts/Level4SunriseCutscene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Level4SunriseCutscene : MonoBehaviour
+    {
+        public void OnEnable()
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}
