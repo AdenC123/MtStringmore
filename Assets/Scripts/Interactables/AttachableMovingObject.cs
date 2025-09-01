6cbdb873f88124b7f265fd1bca9197ac3bdd9321
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2025-08-31 18:48:09 -0700

    Tune zipper perfect release (#163)
    * use distance instead of percent
    
    * bruh

--- Assets/Scripts/Interactables/AttachableMovingObject.cs
+++ Assets/Scripts/Interactables/AttachableMovingObject.cs
@@ -76,8 +76,8 @@ namespace Interactables
 
         [Tooltip("Path renderer")] public MovingObjectPathRenderer pathRenderer;
         
-        [SerializeField, Tooltip("The percent threshold to count as a perfect release"), Range(0, 1)]
-        private float perfectReleaseThreshold = 0.75f;
+        [SerializeField, Tooltip("The distance from the end of the zipper to count as a perfect release"), Min(0)]
+        private float perfectReleaseDistance;
 
         [SerializeField, Tooltip("Audio clip to play when player releases from moving object at the perfect threshold")]
         private AudioClip perfectReleaseClip;
@@ -148,8 +148,7 @@ namespace Interactables
         ///
         /// Counted as distance greater than threshold.
         /// </summary>
-        private bool IsPerfectRelease => DistanceAlongPath >=
-                                         Vector2.Distance(firstPosition, secondPosition) * perfectReleaseThreshold;
+        private bool IsPerfectRelease => Vector2.Distance(_rigidbody.position, secondPosition) <= perfectReleaseDistance;
 
         /// <summary>
         /// Evaluates the velocity at a specific time since motion start.
@@ -182,13 +181,11 @@ namespace Interactables
                 time += Time.fixedDeltaTime;
                 _animator.SetBool(AnimatorHashWhite, IsPerfectRelease);
             }
-            
-            //Reset colors
-            _animator.SetBool(AnimatorHashWhite, false);
 
             _rigidbody.position = secondPosition;
             _rigidbody.velocity = Vector2.zero;
             yield return new WaitForSeconds(exitDelayTime);
+            _animator.SetBool(AnimatorHashWhite, false);
             _prevVelocity = _rigidbody.velocity;
             _player.StopInteraction(this);
             _audioSource.PlayOneShot(badReleaseClip);
@@ -300,6 +297,7 @@ namespace Interactables
             if (_unzippedMotion != null) StopCoroutine(_unzippedMotion);
             _unzippedMotion = StartCoroutine(UnzipCoroutine());
             player.CurrentInteractableArea = null;
+            _animator.SetBool(AnimatorHashWhite, false);
         }
     
         /// <summary>
