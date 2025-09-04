999fcc3dbfc6a90f22d8fe0f59971f590161ab58
AdenC123 <adenrudy23@gmail.com>
2025-09-03 21:27:34 -0700

    CanEditMultipleObjects

--- Assets/Editor/LetterBlockVisualEditor.cs
+++ Assets/Editor/LetterBlockVisualEditor.cs
@@ -7,7 +7,7 @@ namespace Editor
     /// <summary>
     /// Custom editor for letter blocks to easily randomize them.
     /// </summary>
-    [CustomEditor(typeof(LetterBlockVisual))]
+    [CustomEditor(typeof(LetterBlockVisual)), CanEditMultipleObjects]
     public class LetterBlockVisualEditor : UnityEditor.Editor
     {
         public override void OnInspectorGUI()
